b448942315d5bde73d63ef01e1736d5fec631aa7
panonthonlaw-dev <panonthon.law@gmail.com>
2025-12-20 15:59:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ if st.session_state['page'] == 'student':
         
         st.markdown("### 📸 ถ่ายรูปรถ (2 มุม)")
         col_img1, col_img2 = st.columns(2)
-        photo1 = col_img1.file_uploader("1. รูปด้านหน้า (เห็นทะเบียน)", type=['jpg','png','jpeg'], key="p1")
+        photo1 = col_img1.file_uploader("1. รูปด้านหลัง (เห็นทะเบียน)", type=['jpg','png','jpeg'], key="p1")
         photo2 = col_img2.file_uploader("2. รูปด้านข้าง/เต็มคัน", type=['jpg','png','jpeg'], key="p2")
         
         # ปุ่มส่งข้อมูล
