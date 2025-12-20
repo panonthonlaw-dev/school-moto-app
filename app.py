80f9fec3937e4f91be3492e2864d9cac3708032e
panonthonlaw-dev <panonthon.law@gmail.com>
2025-12-20 11:24:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def upload_to_drive(file_obj, filename):
 
 # --- หน้าเว็บ ---
 st.set_page_config(page_title="ทะเบียนรถ รร.", page_icon="🛵")
-st.title("🛵 ระบบลงทะเบียนรถจักรยานยนต์")
+st.title("🛵 ระบบลงทะเบียนรถจักรยานยนต์พัฒวิทย์")
 
 # สร้างเมนู
 menu = st.sidebar.radio("เมนู", ["📝 นักเรียนลงทะเบียน", "👮 ครูตรวจสอบ"])
